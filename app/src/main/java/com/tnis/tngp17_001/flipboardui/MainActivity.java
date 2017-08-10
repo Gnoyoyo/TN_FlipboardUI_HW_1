140de8252047f140b26fb5ed63bda9be35194821
Gno <genoyoyo@gmail.com>
2017-08-10 14:34:05 +0700

    Initial Comit

---
+++ app/src/main/java/com/tnis/tngp17_001/flipboardui/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tnis.tngp17_001.flipboardui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
